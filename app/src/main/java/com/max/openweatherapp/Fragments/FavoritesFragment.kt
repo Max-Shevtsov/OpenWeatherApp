00a7fd85d81435b673faaee9fa556f6a04d1dbb7
Maxim Shevtcov <113129988+Max-Shevtsov@users.noreply.github.com>
2024-04-26 08:21:54 +0000

    put swipeRefresh functionality into FavoritesFragment

--- app/src/main/java/com/max/openweatherapp/Fragments/FavoritesFragment.kt
+++ app/src/main/java/com/max/openweatherapp/Fragments/FavoritesFragment.kt
@@ -3,6 +3,8 @@ class FavoritesFragment: Fragment(R.layout.favorites_fragment) {
     private val _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var adapter: WeatherBroadcastsAdapter
+
     override fun onCreateView(
         inflater: layoutInflater, 
         container: ViewGroup?,
@@ -10,11 +12,43 @@ class FavoritesFragment: Fragment(R.layout.favorites_fragment) {
         ): View? {
             _binding = ActivityMainBinding.inflate(inflater, container, false)
             val view = binding.root
+            
+            adapter = WeatherBroadcastsAdapter { cityId ->
+                viewModel.deleteCityFromDb(cityId)
+            }
+            
+            val recyclerView = binding.recyclerView
+            recyclerView.adapter = adapter
+            recyclerView.layoutManager = LinearLayoutManager(this)
+
+            initListeners()
+            renderState()
+
             return view
         }
 
     override fun onViewCreated(view: View, sevedInstanceState: Bundle?) {
     
+    }
+
+    private fun renderState() {
+        lifecycleScope.launch(Dispatchers.Main) {
+            viewModel.uiState.collect { state ->
+                adapter.submitList(state.allCity)
+                if (!state.isLoading)
+                    binding.swipeRefresh.isRefreshing = false
+                //if(state.errorMessage? != null)run {
+                //val toast = Toast.makeText(context, state.errorMessage,).show()
+            }
+        }
+    }
+
+    private fun initListeners() {
+        binding.swipeRefresh.setOnRefreshListener {
+            Log.e("!!!", "onRefresh called from SwipeRefreshLayout")
+            viewModel.updateWeatherBroadcast()
+            
+        }
     }
 
     overrid fun onDestroyView() {
--- app/src/main/java/com/max/openweatherapp/MainActivity.kt
+++ app/src/main/java/com/max/openweatherapp/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         MainViewModelFactory((application as CityApplication).repository)
     }
 
-    private lateinit var adapter: WeatherBroadcastsAdapter
+    
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,22 +38,12 @@ class MainActivity : AppCompatActivity() {
 
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
-
-        adapter = WeatherBroadcastsAdapter { cityId ->
-            viewModel.deleteCityFromDb(cityId)
-        }
-
-        val recyclerView = binding.recyclerView
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
         val intent = intent
-                }
-
+        
         setContentView(view)
         setSupportActionBar(binding.toolBar)
         handleIntent(intent)
-        initListeners()
-        renderState()
+        
 
     }
 
@@ -71,25 +61,7 @@ class MainActivity : AppCompatActivity() {
             }
         } 
 
-    private fun renderState() {
-        lifecycleScope.launch(Dispatchers.Main) {
-            viewModel.uiState.collect { state ->
-                adapter.submitList(state.allCity)
-                if (!state.isLoading)
-                    binding.swipeRefresh.isRefreshing = false
-                //if(state.errorMessage? != null)run {
-                //val toast = Toast.makeText(context, state.errorMessage,).show()
-            }
-        }
-    }
-
-    private fun initListeners() {
-        binding.swipeRefresh.setOnRefreshListener {
-            Log.e("!!!", "onRefresh called from SwipeRefreshLayout")
-            viewModel.updateWeatherBroadcast()
-            
-        }
-    }
+    
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.options_menu, menu)
