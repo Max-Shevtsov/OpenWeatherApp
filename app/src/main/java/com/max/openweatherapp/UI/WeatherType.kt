8c64576a6f66b9e7458b6c1cdfcbdd11b23218ee
Maxim Shevtcov <113129988+Max-Shevtsov@users.noreply.github.com>
2024-05-07 14:21:42 +0000

    add enum for weatherType

---
+++ app/src/main/java/com/max/openweatherapp/UI/WeatherType.kt
@@ -0,0 +1,14 @@
+package com.max.openweatherapp.UI
+
+enum class weatherType {
+    NO = "no"
+    CLEAR_SKY = "clear sky",
+    FEW_CLOUDS = "	few clouds",
+    SCATTERED_CLOUDS = "scattered clouds",
+    BROKEN_CLOUDS = "broken clouds",
+    SHOWER_RAIN = "shower rain",
+    RAIN = "rain",
+    THUNDERSTROM = "thunderstorm",
+    SNOW = "snow",
+    MIST = "mist"
+}
