808fb581cc57fd2865eeb845a4dd9b7c0dd55a93
Maxim Shevtcov <113129988+Max-Shevtsov@users.noreply.github.com>
2024-06-07 07:35:40 +0000

    add local and network dataSources

--- app/src/main/java/com/max/openweatherapp/data/CityRepository.kt
+++ app/src/main/java/com/max/openweatherapp/data/CityRepository.kt
@@ -1,11 +1,13 @@
 package com.max.openweatherapp.data
 
 import kotlinx.coroutines.flow.Flow
-
-class CityRepository {
-
+@Singleton
+class CityRepository(
     private val cityLocalDataSource: CityDao
-    private val networkDataSource: NetworkDataSource
+    private val networkDataSource: WeatherApiService  // пеерименовать в NetworkDataSource
+) {
+
+    
 
     fun cities(): Flow<List<City>> {
         return cityDao.getAlphabetizedCity()
@@ -31,5 +33,27 @@ class CityRepository {
         return cityDao.getCityById(cityId)
     }
 
+    suspend fun getWeatherBroadcast(city: String): City {
+        Log.e("!!!", "Start loading")
 
+        val coordinates = networkDataSource.getCoordinatesOfCity(city)
+        Log.e("!!!", "Coordinates:$coordinates")
+
+        val result = networkDataSource.getBroadcast(
+            coordinates.firstOrNull()?.lat,
+            coordinates.firstOrNull()?.lon,
+        )
+
+        Log.e("!!!", "City`s: $city")
+
+        val city = City(
+            cityName = city,
+            cityLat = coordinates.first().lat,
+            cityLon = coordinates.first().lon,
+            cityTemp = kelvinToCelsiusConverter(result.weatherParamsResponse.temp),
+            cityWindSpeed = "${result.windResponse.speed} М/С",
+            icon = result.weatherTypeInformation.first().icon,
+        )
+        return city
+    }
 }
