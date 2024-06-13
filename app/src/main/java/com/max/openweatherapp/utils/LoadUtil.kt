46c72614b84599dc3c5f7609675499dd43337b64
Maxim Shevtcov <113129988+Max-Shevtsov@users.noreply.github.com>
2024-06-13 07:35:00 +0000

    change names of extention functions

--- app/src/main/java/com/max/openweatherapp/utils/LoadUtil.kt
+++ app/src/main/java/com/max/openweatherapp/utils/LoadUtil.kt
@@ -9,7 +9,3 @@ fun loadWeatherTypePicture(icon: String?, uiItem: ImageView) {
     uiItem.load(url)
 }
 
-fun kelvinToCelsiusConverter(kelvinTemp: Double): String {
-    val KELVIN_TO_CELSIUS = 273.15
-    return "${(kelvinTemp - KELVIN_TO_CELSIUS).toUInt()} C"
-}
