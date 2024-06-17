6a59c001561cf6ed451d7cf1949ac0eba9511a09
Max <maxschew@yandex.ru>
2024-06-17 14:36:24 +0200

    add CityToFavoriteMapper

---
+++ app/src/main/java/com/max/openweatherapp/data/mappers/CityToFavoriteMapper.kt
@@ -0,0 +1,31 @@
+package com.max.openweatherapp.data.mappers
+
+import com.max.openweatherapp.data.room.cityDataSource.City
+import com.max.openweatherapp.data.room.favoritesDataSource.FavoriteCity
+// mapper (easy to test)
+class CityToFavoriteMapper : (City) -> FavoriteCity {
+
+    override fun invoke(city: City): FavoriteCity {
+        return FavoriteCity(
+            cityId = city.cityId,
+            cityName = city.cityName,
+            cityTemp = city.cityTemp,
+            cityWindSpeed = city.cityWindSpeed,
+            cityLat = city.cityLat,
+            cityLon = city.cityLon,
+            icon = city.icon,
+        )
+    }
+}
+//extention static fun
+fun City.toFavoriteCity(): FavoriteCity {
+    return FavoriteCity(
+        cityId = cityId,
+        cityName = cityName,
+        cityTemp = cityTemp,
+        cityWindSpeed = cityWindSpeed,
+        cityLat = cityLat,
+        cityLon = cityLon,
+        icon = icon,
+    )
+}
