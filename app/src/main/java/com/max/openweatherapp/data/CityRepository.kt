ad6bf94218d3026c85bbc2ad8a581b8d60c530bf
Max <maxschew@yandex.ru>
2024-06-26 11:18:08 +0200

    fun getCity: make type Flow<List<City>>

--- app/src/main/java/com/max/openweatherapp/data/CityRepository.kt
+++ app/src/main/java/com/max/openweatherapp/data/CityRepository.kt
@@ -6,27 +6,19 @@ import com.max.openweatherapp.data.room.cityDataSource.City
 import com.max.openweatherapp.data.room.cityDataSource.CityDao
 import kelvinToCelsiusConverter
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
 
 class CityRepository(
     private val localDataSource: CityDao,
     private val networkDataSource: WeatherApiService,  // пеерименовать в NetworkDataSource
 ) {
-    private suspend fun insert(city: City) {
-        localDataSource.insert(city)
-    }
 
-    private suspend fun update(city: City) {
-        localDataSource.update(city)
-    }
 
 //    private suspend fun deleteById(cityId:Long) {
 //        localDataSource.deleteById(cityId)
 //    }
 
-    fun get(): Flow<City> {
+    fun getCity(): Flow<List<City>> {
         return localDataSource.get()
     }
     private suspend fun databaseIsEmpty():Boolean {
@@ -42,33 +34,40 @@ class CityRepository(
         }
     }
 
-    suspend fun getWeatherBroadcast(city: String) {
+    suspend fun searchWeatherByCityName(city: String) {
 
         Log.e("!!!", "Start loading")
 
         val coordinates = networkDataSource.getCoordinatesOfCity(city)
         Log.e("!!!", "Coordinates:$coordinates")
 
-        val result = networkDataSource.getBroadcast(
+        val weather = networkDataSource.getBroadcast(
             coordinates.firstOrNull()?.lat,
             coordinates.firstOrNull()?.lon,
         )
 
         Log.e("!!!", "City`s: $city")
 
-        val updatedCityId = get().firstOrNull()?.cityId?.plus(1)
+       // val updatedCityId = this.getCity().firstOrNull()?.cityId?.plus(1)
 
         val updatedCity = City(
-            key = 0L,
-            cityId = updatedCityId?:0,
+            cityId = 1,
             cityName = city,
             cityLat = coordinates.first().lat,
             cityLon = coordinates.first().lon,
-            cityTemp = kelvinToCelsiusConverter(result.weatherParamsResponse.temp),
-            cityWindSpeed = "${result.windResponse.speed} М/С",
-            icon = result.weatherTypeInformation.first().icon,
+            cityTemp = kelvinToCelsiusConverter(weather.weatherParamsResponse.temp),
+            cityWindSpeed = "${weather.windResponse.speed} М/С",
+            icon = weather.weatherTypeInformation.first().icon,
         )
         putInDatabase(updatedCity)
+    }
+
+    private suspend fun insert(city: City) {
+        localDataSource.insert(city)
+    }
+
+    private suspend fun update(city: City) {
+        localDataSource.update(city)
     }
 
 }
